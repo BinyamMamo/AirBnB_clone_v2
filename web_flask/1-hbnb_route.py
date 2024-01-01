4b88f3598874e75eb77ef5b0c33ec74e5039ab1c
BinyamMamo <binyammamo01@gmail.com>
2024-01-01 19:13:46 +0400

    Mon Jan  1 19:13:46 +04 2024

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,14 +11,14 @@ app = Flask(__name__)
 def hello_hbnb():
     """return the string "Hello, HBNB!".
     """
-    return ("Hello, HBNB!")
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """return the string "HBNB"
     """
-    return ("HBNB")
+    return "HBNB"
 
 
 if __name__ == "__main__":
