8bb272081160dcc3bfbdce9921c66fb57cbc34ee
BinyamMamo <binyammamo01@gmail.com>
2024-01-02 00:50:43 +0400

    Tue Jan  2 00:50:43 +04 2024

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -5,9 +5,12 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /hbnb: HBnB home page.
 """
-from models import storage
 from flask import Flask
 from flask import render_template
+from models import storage
+from models.amenity import Amenity
+from models.place import Place
+from models.state import State
 
 app = Flask(__name__)
 
@@ -15,9 +18,9 @@ app = Flask(__name__)
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Renders the 100-hbnb.html template."""
-    states = storage.all("State")
-    amenities = storage.all("Amenity")
-    places = storage.all("Place")
+    amenities = storage.all(Amenity)
+    places = storage.all(Place)
+    states = storage.all(State)
     return render_template("100-hbnb.html",
                            states=states, amenities=amenities, places=places)
 
@@ -29,4 +32,4 @@ def teardown(exc):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000, debug=True)
