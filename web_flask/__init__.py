ba79a3344eb36dc1bbc87a8842c175de118c3664
BinyamMamo <binyammamo01@gmail.com>
2024-01-01 18:57:33 +0400

    Mon Jan  1 18:57:33 +04 2024

---
+++ web_flask/__init__.py
@@ -0,0 +1,7 @@
+from web_flask import 0-hello_route
+from web_flask import 1-hbnb_route
+from web_flask import 2-c_route
+from web_flask import 3-python_route
+from web_flask import 4-number_route
+from web_flask import 5-number_template
+from web_flask import 6-number_odd_or_even
